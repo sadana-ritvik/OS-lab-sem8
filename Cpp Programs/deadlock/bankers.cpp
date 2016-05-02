a9f68b7ff9fa41fcd0b9addfe5bbfc2f07c5da02
sadana-ritvik <ritviksadana@gmail.com>
2016-05-03 00:27:04 +0530

    Disk Schduling

--- Cpp Programs/deadlock/bankers.cpp
+++ Cpp Programs/deadlock/bankers.cpp
@@ -49,45 +49,46 @@ void main()
             process = -1;
             for(i = 0; i < p; i++)
             {
-if(completed[i] == 0)//if not completed
-{
-    process = i ;
-    for(j = 0; j < r; j++)
-    {
-        if(avail[j] < need[i][j])
-        {
-            process = -1;
-            break;
-        }
-    }
-}
-if(process != -1)
-    break;
-}
+                if(completed[i] == 0)//if not completed
+                {
+                    process = i ;
+                    for(j = 0; j < r; j++)
+                    {
+                        if(avail[j] < need[i][j])
+                        {
+                            process = -1;
+                            break;
+                        }
+                    }
+                }
+                if(process != -1)
+                    break;
+            }
 
-if(process != -1)
-{
-    printf(“\nProcess %d runs to completion!”, process + 1);
-    safeSequence[count] = process + 1;
-    count++;
-    for(j = 0; j < r; j++)
-    {
-        avail[j] += alloc[process][j];
-        alloc[process][j] = 0;
-        Max[process][j] = 0;
-        completed[process] = 1;
-    }
-}
-}while(count != p && process != -1);
-if(count == p)
-{
-    printf(“\nThe system is in a safe state!!\n”);
-    printf(“Safe Sequence : < “);
-    for( i = 0; i < p; i++)
-        printf(“%d “, safeSequence[i]);
-    printf(“>\n”);
-}
-else
-    printf(“\nThe system is in an unsafe state!!”);
-getch();
+            if(process != -1)
+            {
+                printf(“\nProcess %d runs to completion!”, process + 1);
+                safeSequence[count] = process + 1;
+                count++;
+                for(j = 0; j < r; j++)
+                {
+                    avail[j] += alloc[process][j];
+                    alloc[process][j] = 0;
+                    Max[process][j] = 0;
+                    completed[process] = 1;
+                }
+            }
+        }while(count != p && process != -1);
+        
+        if(count == p)
+        {
+            printf(“\nThe system is in a safe state!!\n”);
+            printf(“Safe Sequence : < “);
+            for( i = 0; i < p; i++)
+                printf(“%d “, safeSequence[i]);
+            printf(“>\n”);
+        }
+        else
+            printf(“\nThe system is in an unsafe state!!”);
+        
 }
